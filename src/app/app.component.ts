125f3a9c9889c3ff06d74b7ad9c90bad8ff738ad
Shawn Kastner <Shawnkastner@hotmail.de>
2023-03-10 10:11:28 +0100

    added images and slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'shawnkastner.de';
+export class AppComponent implements OnInit {
+  images = ['laptop.jpg', 'codingComputer.jpg', 'laptop.jpg'];
+  currentImage = 0;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+    }, 8000)
+  }
 }
