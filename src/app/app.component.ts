056d1bcda6ae2c82c6d3c235d6b1366a29cb9379
Shawn Kastner <Shawnkastner@hotmail.de>
2023-03-10 10:59:27 +0100

    added headlines and fadeIn animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   images = ['laptop.jpg', 'codingComputer.jpg', 'academy.jpg'];
+  headlines = ['Bring engineering to the next level',
+    'Born to code',
+    'Graduated at TUM'
+  ];
   currentImage = 0;
   showImage = true;
 
